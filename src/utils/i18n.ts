16d1ce13f2ff239a1c23bea6e9dada4c24d4aee3
LinuxCat <masch77.linuxcat@gmail.com>
2026-02-22 02:56:30 +0100

    Update translate

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,18 +1,15 @@
 import en from './i18n/en.json' with { type: 'json' };
 import cn from './i18n/cn.json' with { type: 'json' };
 
+// This is genuinely the worst function i've ever written i think
 export default function translate(str : string, language: "en" | "cn") : string{
     const lang = language == 'cn' ? cn : en;
-    let last : unknown = lang; // This is the worst thing ever
 
     try {
         const keys = str.split(".");
-        keys.forEach(k => {
-            last = last[k];
-        })
+        return keys.reduce<unknown>((prev, cur) => (prev as {[k: string] : string})[cur], lang) as string ?? str;
     } catch {
         return str;
     }
 
-    return (last as string) ?? str; 
 }
