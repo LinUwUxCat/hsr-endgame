36293a23256d41cbcac12d2a0248d5b596d23a4b
LinuxCat <masch77.linuxcat@gmail.com>
2026-02-23 02:25:03 +0100

    Missing id

--- src/utils/merge.ts
+++ src/utils/merge.ts
@@ -4,6 +4,7 @@ export function mergeById<T extends object>(obj1: T[], obj2: T[], id: keyof T = 
     return obj1.map(item => {
         let found = obj2.find((v) => v[id] === item[id]);
         if (found == undefined) return item;
+        console.log(found)
         return merge(item, found);
     })
 }
