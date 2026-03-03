87ee41423e4cb1fded0e920c1c20a7a4b9bc1fe6
LinuxCat <masch77.linuxcat@gmail.com>
2026-03-03 06:42:50 +0100

    Main

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,9 +7,8 @@ export default function HomePage(): ReactElement {
         <div style={{display: "flex", flexDirection: "column"}}>
             <Ts>home.warning</Ts>
             <div>Use the navbar to navigate.</div>
-            <div>There is a lot of missing data here. Most monster images and names are missing.</div>
-            <div>That said, HP values should be correct for anything pre-4.0.</div>
-            <div>There will be a "missing data" line wherever HP is incorrect.</div>
+            <div>Some data, like Buffs/Effects (in english) is still missing.</div>
+            <div>The AS Node 2 Buff data is missing in chinese as well.</div>
             <div>If you know of a way to extract endgame data from the game directly, please message @linuxcat on discord.</div>
         </div>
     )
